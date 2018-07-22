f9ebfd0f909e502b7b7e1cc99b14b6c371c51fcc
rafskov <rskovron@gmail.com>
2018-07-21 20:50:21 -0700

    added session details to session page, session variables in send route

--- app.py
+++ app.py
@@ -8,15 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/send',methods=['GET','POST'])
+@app.route('/send',methods=['POST','GET'])
 
 def send():
     if request.method =='POST':
         session_rank = request.form['overall']
+        session_beach= request.form['spots']
+        session_swell= request.form['swell']
+        session_wind= request.form['wind']
+        session_tide=request.form['tide']
+        return render_template('session.html',session_rank=session_rank,session_tide=session_tide,
+        session_beach=session_beach,session_swell=session_swell,session_wind=session_wind)
 
-        return render_template('session.html',session_rank=session_rank)
-
-    return render_template('index.html')
+    return render_template('session.html')
 
 
 
