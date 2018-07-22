42cd338e8f0599c23641db58a437cde58f2a4370
rafskov <rskovron@gmail.com>
2018-07-21 23:43:11 -0700

    hoooked up sqlite database, fixed submit button, added running log

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:////Users/rws/Documents/Creative/surfdiary-project/session.db' #config file should be separate, tells sqlal how to connect
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
 
+#create a table in database so that python classes can be mapped to table
+#to record each session
+
+class Sessions(db.Model):
+    #create columns
+    id = db.Column(db.Integer,primary_key=True)
+    beach = db.Column(db.String(100))
+    swell = db.Column(db.String(3))
+    wind = db.Column(db.String(3))
+    rank = db.Column(db.String(100))
+    tide = db.Column(db.String(50))
+    #translates table
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    result = Sessions.query.all() #return results in sessions table in a list of sqlacl objects
+
+    return render_template('index.html',result=result)
 
 @app.route('/send',methods=['POST','GET'])
 
@@ -17,10 +37,15 @@ def send():
         session_swell= request.form['swell']
         session_wind= request.form['wind']
         session_tide=request.form['tide']
-        return render_template('session.html',session_rank=session_rank,session_tide=session_tide,
-        session_beach=session_beach,session_swell=session_swell,session_wind=session_wind)
 
-    return render_template('session.html')
+        session_update = Sessions(beach=session_beach,swell=session_swell,wind=session_wind,tide=session_tide,rank=session_rank)
+        db.session.add(session_update)
+        db.session.commit()
+
+        #return redirect(url_for('session_log')) #dont return view or template
+        result = Sessions.query.all() 
+        return render_template('session.html',result=result)
+
 
 
 
